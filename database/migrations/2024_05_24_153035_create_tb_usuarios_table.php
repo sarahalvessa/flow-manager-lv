fa5dc9ba914cc2ebfef4924d01e1dacd80147744
sarahalvessa <sarah_030301@outlook.com>
2024-05-24 13:06:52 -0300

    refactor: ajuste nas migrations

--- database/migrations/2024_05_24_153035_create_tb_usuarios_table.php
+++ database/migrations/2024_05_24_153035_create_tb_usuarios_table.php
@@ -12,7 +12,7 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('tb_usuarios', function (Blueprint $table) {
-            $table->id();
+            $table->id('usuario_id');
             $table->string('nome');
             $table->string('email')->unique();
             $table->string('senha');
